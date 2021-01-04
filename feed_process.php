0ec7c0fd810fbca5cec6acc35aac596f2c4793f8
shadin710 <shadin710@gmail.com>
2021-01-04 15:55:34 +0600

    Fixed some minor bugs

--- feed_process.php
+++ feed_process.php
@@ -1,5 +1,5 @@
 <?php
-    
+    session_start();
     include_once 'includes/db_connection.php';
 
     if($_SERVER['REQUEST_METHOD']=='POST')
@@ -16,17 +16,17 @@
         $salary = $_POST['salary'];
         $exper = $_POST['exper'];
         $type_time = $_POST['type_time'];
-
+        $summary = $_POST['feed_post'];
         $qua = $_POST['qua'];
+        $owner_id = $_SESSION['owner_id'];
 
-
-        if($job_count>0)
-        {
-            echo "The job already exists!!";
+        //if($job_count>0)
+        //{
+          //  echo "The job already exists!!";
             //header("Location: feed.php");
-        }
-        else
-        {
+        //}
+       // else
+        //{
             $sql = "INSERT INTO post_job (comName,position,comAddress,responsibility,skill,skill1,skill2,skill3,skill4, salary,exper,type_time) VALUES ('$comName','$position','$comAddress','$res','$skill0','$skill1','$skill2','$skill3','$skill4','$salary','$exper','$type_time')";
 
             if(!mysqli_query($conn,$sql))
@@ -35,9 +35,24 @@
             }
             else
             {
-                header('Location:homepage.php');
+                
+
+                $sql_search_id = "SELECT * FROM post_job WHERE comName = '$comName' and position = '$position' and comAddress='$comAddress'";
+                $result_id = mysqli_query($conn,$sql_search_id) Or die("Failed to query " . mysqli_connect_error());
+                $result = mysqli_fetch_assoc($result_id);
+                $job_id = $result['id'];
+
+                $info_owner = "INSERT INTO notify_owner (job_id,owner_id,summary) VALUES ('$job_id','$owner_id','$summary')";
+                if (!mysqli_query($conn,$info_owner)) 
+                {
+                    die("Failed to insert into notify owner " . mysqli_error($conn));
+                }
+                else
+                {
+                    header('Location:homepage.php');
+                }
             }
-        }
+        //}
     }
 
     mysqli_close($conn);
