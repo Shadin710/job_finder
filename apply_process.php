c0b5497f66ad5a264f3db1e4c5b15c75dace5989
shadin710 <shadin710@gmail.com>
2021-01-05 17:41:25 +0600

    Added a new table

--- apply_process.php
+++ apply_process.php
@@ -11,6 +11,17 @@
         $result = mysqli_query($conn,$sql_apply) Or die("Failed to query " . mysqli_error($conn));
         $row_user = mysqli_fetch_assoc($result);
         $user_id = $row_user['id'];
+        $req_name = $row_user['username'];
+        $req_skill0 =$row_user['skill0'];
+        $req_skill1 =$row_user['skill1'];
+        $req_skill2 =$row_user['skill2'];
+        $req_skill3 =$row_user['skill3'];
+        $req_skill4 =$row_user['skill4'];
+        $req_occupation =$row_user['occupation'];
+        $req_country = $row_user['country'];
+        $req_email=$row_user['email'];
+
+        $sql_request = "INSERT INTO request (u_id,username,occupation,skill0,skill1,skill2,skill3,skill4,country,email) VALUES('$user_id','$req_name','$req_occupation','$req_skill0','$req_skill1','$req_skill2','$req_skill3','$req_skill4','$req_country','$req_email')";
 
         $sql_aj = "SELECT * FROM post_job WHERE id = '$id'";
         $result_job = mysqli_query($conn,$sql_aj) Or die("Failed to query " . mysqli_query($conn));
@@ -25,8 +36,15 @@
         }
         else
         {
+            if (!mysqli_query($conn,$sql_request)) 
+            {
+                die("Failed to insert the data " . mysqli_error($conn));
+            }
+            else
+            {
+                header("Location:Notification.php");
+            }
             
-            header("Location:Notification.php");
         }
     }
 ?>
