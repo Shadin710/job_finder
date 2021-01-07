a4246d5cd34205ff3d971bc3252cf94c1c66e9df
shadin710 <shadin710@gmail.com>
2021-01-07 23:05:27 +0600

    Added ignore report option

---
+++ delete_report.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+
+    if (empty($_SESSION['admin_name'])) 
+    {
+        header("Location:admin_login.php");
+    }
+    $id = $_SESSION['job_id'];
+    $sql_del = "DELETE FROM report_job WHERE job_id = '$id'";
+    if (!mysqli_query($conn,$sql_del)) 
+    {
+        die("Failed to delete from report " . mysqli_error());
+    }
+    else
+    {
+        header("Location:admin_dashboard.php");
+    }
+?>
